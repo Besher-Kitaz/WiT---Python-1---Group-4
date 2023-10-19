9526eaf52f6fc23dbdb3b29adfb9deb534aadba4
Besher-Kitaz <127769566+Besher-Kitaz@users.noreply.github.com>
2023-10-20 00:30:40 +0300

    Add files via upload

---
+++ Assignments/Session 2/Discount.py
@@ -0,0 +1,9 @@
+"""A shop has creare a new discount offer. It is as follows;
+Every item you buy in the shop, you have a 10% discount.
+
+However, the retail employee, Sofia, is struggling to calculate the discount. Write a program to help her"""
+
+# Inpout: The input is the price without the discount
+# Output: The price with the discount
+
+"""You should """
---
+++ Assignments/Session 2/Jimmy_Problem.py
@@ -0,0 +1,16 @@
+"""Please Edit the code below as instructed to"""
+
+"""
+Jimmy would like to build a calculator that takes two inputs and returns their division. 
+He built the following code: 
+"""
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print(num1 // num2)
+
+"""However, Jimmy faced a problem. Whenever he enters a decimal number, the program results in an error (Traceback).
+For example, if he entered 2.5, the calculator will give an error."""
+
+# Change the code that Jimmy wrote to fix the problem
