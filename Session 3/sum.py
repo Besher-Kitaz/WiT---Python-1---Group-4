c430e865a94a3b206d4684ff4e957834bbc62254
Besher-Kitaz <127769566+Besher-Kitaz@users.noreply.github.com>
2023-10-26 17:27:39 +0300

    Add files via upload

---
+++ Session 3/sum.py
@@ -0,0 +1,18 @@
+""""
+    We would to create a code that calculates the sum, from 0 to 10.
+    We can achieve this using the while loop. Complete the code below to achieve the task.
+"""
+
+# first, we need a counter for our loop (iteration variable)
+
+i = 0
+
+# Now we need a variable to store the sum inside it. We assin 0 to it, because at the beginning, we have nothing. 
+# Then, as the while starts working, we add the value of i to it.
+
+sum = 0
+
+# Continue from here. Create the while loop and calculate the sum
+
+
+print(f"sum = {sum}")
