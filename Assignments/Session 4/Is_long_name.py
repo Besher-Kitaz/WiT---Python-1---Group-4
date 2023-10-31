cd601170649783d4f2d1f35b5ce419f162b72641
Besher-Kitaz <127769566+Besher-Kitaz@users.noreply.github.com>
2023-11-01 02:13:48 +0300

    Add files via upload

---
+++ Assignments/Session 4/Is_long_name.py
@@ -0,0 +1,11 @@
+"""We have a list of people's names that we want to filter. If the name if the user is longer than 4 characters, store it in a different a new list"""
+
+names = ["John", "James", "Mary", "Adam", "Ali", "Sarah", "George", "Mohammed", "Jeff", "Amal"]
+
+# We now need to create a list of people whose names are longer than 4 characters. At first, the list is empty
+
+long_names = []
+
+#Now, we iterate over the "names" list and check if a name is longer than 4 characters, then we add it to the other list.
+
+# Write your for loop below here.
---
+++ Assignments/Session 4/Reverse_loop.py
@@ -0,0 +1,15 @@
+"""We have seen examples of programmes that print out the numbers from 0 to 10 (for example).
+However, we would like now to reverse the order to make it decending. from 10 to 0 (for example)."""
+
+"""Write a programme that prints out the numbers from n to 0, where you take n from the input. Pleaes follow the following steps"""
+
+# First, we need to take the input for n.
+"""Remove this comment and write code here"""
+
+# Then, we need while (or for) loop. How will the condition (or range, in case of for loop) look like? 
+"""Remove this comment and write code here for the loop"""
+
+# Hint: If you decide to use a while loop, what going to happen to n at every iteration (cycle)?
+
+
+
